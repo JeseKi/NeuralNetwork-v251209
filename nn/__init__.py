827bdea497f3e3ea9aa8d408686a9db8bc4a1c6f
JeseKi <2094901072@qq.com>
2025-12-09 11:17:53 +0800

    feat: 初始化模块中加入神经网络

---
+++ nn/__init__.py
@@ -0,0 +1,6 @@
+from nn.nn import NeuralNetwork, NeuralModule
+from nn.train import backward, train
+from nn.loss import LossType
+from nn.activation import ActivationType
+
+__all__ = ["NeuralNetwork", "NeuralModule", "backward", "train", "LossType", "ActivationType"]
