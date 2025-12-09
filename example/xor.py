435d83a962b73c2d9d37a2218b6a9f78502c6f21
JeseKi <2094901072@qq.com>
2025-12-09 11:19:23 +0800

    feat: 引入 xor 作为 example

---
+++ example/xor.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import numpy as np
+
+from nn.nn import NeuralNetwork, NeuralModule
+from nn.train import train
+from nn.loss import LossType
+from nn.activation import ActivationType
+
+INPUT: List[np.ndarray] = [
+    np.array([[0, 0]]),
+    np.array([[0, 1]]),
+    np.array([[1, 0]]),
+    np.array([[1, 1]]),
+]
+
+OUTPUT: List[np.ndarray] = [
+    np.array([[0]]),
+    np.array([[1]]),
+    np.array([[1]]),
+    np.array([[0]]),
+]
+
+neural_network = NeuralNetwork()
+neural_network.add_layer(
+    NeuralModule(input_size=2, output_size=2, activation_type=ActivationType.SIGMOID)
+)
+neural_network.add_layer(
+    NeuralModule(input_size=2, output_size=1, activation_type=ActivationType.SIGMOID)
+)
+
+train(
+    neural_network=neural_network,
+    inputs=INPUT,
+    targets=OUTPUT,
+    loss_type=LossType.MSE,
+    epochs=1000000,
+    record_interval=10000,
+)
+
+for input, target in zip(INPUT, OUTPUT):
+    output = neural_network.forward(input)
+    print(f"Input: {input}, Target: {target}, Output: {output}")
--- nn/__init__.py
+++ nn/__init__.py
@@ -3,4 +3,11 @@ from nn.train import backward, train
 from nn.loss import LossType
 from nn.activation import ActivationType
 
-__all__ = ["NeuralNetwork", "NeuralModule", "backward", "train", "LossType", "ActivationType"]
+__all__ = [
+    "NeuralNetwork",
+    "NeuralModule",
+    "backward",
+    "train",
+    "LossType",
+    "ActivationType",
+]
